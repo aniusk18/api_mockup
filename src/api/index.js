01c14902465e41076e23f681b667952209ab5408
Willy Alvaro Cordon Huanca <wcordon@provincianet.com.ar>
2023-11-02 10:34:24 -0300

    test local

--- src/api/index.js
+++ src/api/index.js
@@ -3,12 +3,25 @@ const express = require('express');
 const emojis = require('./emojis');
 
 const router = express.Router();
+const NosisMockupController = require('../Controllers/NosisMockupController');
+const CrmBancoMockup = require('../Controllers/CrmBancoMockupController');
 
 router.get('/', (req, res) => {
   res.json({
     message: 'API - 👋🌎🌍🌏',
   });
 });
+
+router.get('/alive', NosisMockupController.alive);
+//Nosis
+router.post('/login',NosisMockupController.login);
+router.post('/validacion-nosis',NosisMockupController.validacionNosis);
+router.post('/evaluacion-nosis',NosisMockupController.evaluacionNosis);
+//Crm banco
+router.post('/login',NosisMockupController.login);
+router.post('/get-status-case',CrmBancoMockup.statusCase);
+router.post('/get-case-dni',CrmBancoMockup.getCaseDni);
+
 
 router.use('/emojis', emojis);
 
